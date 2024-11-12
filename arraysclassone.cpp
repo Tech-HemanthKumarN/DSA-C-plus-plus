10b474f091c2c6cd31be259460f5242f3218d6ee
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2024-11-12 21:36:14 +0530

    Update arraysclassone.cpp

--- arraysclassone.cpp
+++ arraysclassone.cpp
@@ -253,35 +253,35 @@
 //TWO POINTER TECHNIQUE VERY IMP
 //FOR ODD
 
-#include<iostream>
-using namespace std;
-void extremePrint(int arr[],int n)
-{
-    int i=0;
-    int j=n-1;
-    while(i<=j)
-    {
-       if(i==j)
-       {
-        cout<<arr[i]<<" ";
-        // i++;
-        break;
-       }
-       else
-       {
-         cout<<arr[i]<<" ";
-        i++;
-        cout<<arr[j]<<" ";
-        j--;
-       }
-    }
-}
-int main()
-{
-    int arr[]={ 10,20,30,40,50,60,70};
-    int size=7;
-    extremePrint(arr,size);
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// void extremePrint(int arr[],int n)
+// {
+//     int i=0;
+//     int j=n-1;
+//     while(i<=j)
+//     {
+//        if(i==j)
+//        {
+//         cout<<arr[i]<<" ";
+//         // i++;
+//         break;
+//        }
+//        else
+//        {
+//          cout<<arr[i]<<" "; 
+//         i++;
+//         cout<<arr[j]<<" ";
+//         j--;
+//        }
+//     }
+// }
+// int main()
+// {
+//     int arr[]={ 10,20,30,40,50,60,70};
+//     int size=7;
+//     extremePrint(arr,size);
+//     return 0;
+// }
 //________________________________________________________________________________________________________
 //HOME WORK SWAPING AN ARRAY IN DIFFERENT WAY, AND REVERSE AN ARRAY
