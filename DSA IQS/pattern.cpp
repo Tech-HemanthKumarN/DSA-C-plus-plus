5798c1b6da8c4e459fd9c4081784737632a9732d
Tech-HemanthKumarN <hemanthnanjappaji@gmail.com>
2024-11-28 23:19:11 +0530

    pattern

---
+++ DSA IQS/pattern.cpp
@@ -0,0 +1,59 @@
+//PATTERNS 
+//SQUARS
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int side;
+//     cout<<"Enter the side:"<<endl;
+//     cin>>side;
+
+//     for(int row=0; row<side; row++)
+//     {
+//         for(int col=0; col<side; col++)
+//         {
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+//_____________________________________________________________________________________________________
+//RECTANGLE
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int length;
+//     int width;
+    
+//     cout<<"Enter the length:"<<endl;
+//     cin>>length;
+//     cout<<"Enter the width:"<<endl;
+//     cin>>width;
+
+//     for(int row=0; row<length; row++)
+//     {
+//         for(int col=0; col<width; col++)
+//         {
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+//_____________________________________________________________________________________________________________
+//INTERVIEW QUESTION FINDING THE UNIQE NUMBER USING XOR
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int ans=(2^2^4^5^6^5^8^4^6);
+//    cout<<ans;
+
+//     return 0;
+// }
+//________________________________________________________________________________________________________
+ 
