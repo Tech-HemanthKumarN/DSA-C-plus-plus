f682e8b36b36c97e940665a5482dd9c6dbaffbf3
Tech-HemanthKumarN <hemanthnanjappaji@gmail.com>
2024-11-28 23:38:15 +0530

    Patterns

---
+++ intro to cpp/Patterns.cpp
@@ -0,0 +1,59 @@
+// //SQUAR PATTREN
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int side;
+//     cout<<"enter the side:"<<endl;
+//     cin>>side;
+
+//     for(int row=0; row<side; row++)
+// it will execute in row manner......................outer loop row
+//     {
+//         for(int col=0; col<side; col++)
+//         {
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+//_________________________________________________________________________________________________________________
+//RECTANGLE PATTERN
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int length;
+//     int width;
+//     cout<<"Enter the length:"<<endl;
+//     cin>>length;
+//     cout<<"Enter the width:"<<endl;
+//     cin>>width;
+
+//     for(int row=0; row<length; row++)
+//     {
+//         for(int col=0; col<width; col++)
+//         {
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+//_____________________________________________________________________________________________________________
+//INTERVIEW QUESTION FINDING THE UNIQE NUMBER USING XOR
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//    int ans=(2^2^4^5^6^5^8^4^6);
+//    cout<<ans;
+
+//     return 0;
+// }
+//________________________________________________________________________________________________________
+ 
