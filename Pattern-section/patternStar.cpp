0edbe1ffde647a91dd382f39422d2a1048d4e528
Hemanthkumar N <hemanthnanjappaji@gmail.com>
2025-03-10 20:46:16 +0530

    PatternStars

--- Pattern-section/patternStar.cpp
+++ Pattern-section/patternStar.cpp
@@ -1,12 +1,70 @@
-#include <iostream>
-#include <string>
-using namespace std;
+//HALLOW SQUARE
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int side;
+//     cout<<"enter the side:"<<endl;
+//     cin>>side;
 
+//     for(int row=0; row<side; row++)
+//     {
+//         for(int col=0; col<side; col++)
+//         {
+//             if(row==0 || row==side-1)
+//             {
+//                 cout<<"* ";
+//             }
+//             else
+//             {
+//                 if(col==0 || col==side-1)
+//                 {
+//                     cout<<"* ";
+//                 }
+//                 else
+//                 {
+//                     cout<<"  ";
+//                 }
+//             }
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+//_____________________________________________________________________________________________________________
+//HALLOW RECTANGLE
+#include<iostream>
+using namespace std;
 int main()
 {
-    for (int i = 0; i <= 5; i++)
+    int length;
+    int width;
+    cout<<"Enter the length:"<<endl;
+    cin>>length;
+    cout<<"Enter the width:"<<endl;
+    cin>>width;
+
+    for(int row=0; row<length; row++)
     {
-        cout << string(i, '*') << endl;
+        for(int col=0; col<width; col++)
+        {
+            if(row==0 || row==length-1)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                if(col==0 || col==width-1)
+                {
+                    cout<<"* ";
+                }
+                else
+                {
+                    cout<<"  ";
+                }
+            }
+        }
+        cout<<endl;
     }
     return 0;
 }
